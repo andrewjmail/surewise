c23ccb47cc712d39029c4a9ddc9010cd15b6e4a8
Andrew Mail <andrewmail@sky.com>
2021-02-05 07:38:20 +0000

    Add dropdown icon

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -24,6 +24,17 @@
         display: block;
     }
 
+    .navigation__category__link::after {
+        display: inline-block;
+        margin-left: .255em;
+        vertical-align: .255em;
+        content: "";
+        border-top: .3em solid;
+        border-right: .3em solid transparent;
+        border-bottom: 0;
+        border-left: .3em solid transparent;
+    }
+
     .navigation__category__link:hover {
         color: black;
         transition: color 1s linear;
